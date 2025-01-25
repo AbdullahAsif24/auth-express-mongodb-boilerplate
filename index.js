55cb51d7bd98eb17b915eee5b93e704530ebdb25
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-01-25 18:18:15 +0500

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(express.json());
 app.use((req, res, next) => {
     
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     next();
     
 });
-app.use(cors({origin:"http://localhost:3000"}))
+app.use(cors({origin:"*"}))
 
 app.use("/auth", authRoutes);
 
