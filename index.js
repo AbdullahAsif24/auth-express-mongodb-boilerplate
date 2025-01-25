6cfb82c2da5b57cff14c47cd5174456b6dca0ea3
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-01-25 12:37:53 +0500

    .

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(cors({origin:"http://localhost:3000"}))
 app.use((req, res, next) => {
-
+    
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-
+    
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     next();
-
+    
 });
+app.use(cors({origin:"http://localhost:3000"}))
 
 app.use("/auth", authRoutes);
 
